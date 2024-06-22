c3dcdf61e797f7872366d2480d7230a370c515c4
menescalnickolas <menescalnickolas@gmail.com>
2024-06-22 21:37:49 +0200

    Added onBackClick to conditional in MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -12,7 +12,8 @@ export const MainView = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   if (selectedMovie) {
-    return <MovieView movie={selectedMovie} />;
+    return (<MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)}/>
+  );
   }
 
   if (movies.length === 0) {
