76a6bd1ec35dedf8344803285420eb2d490928f1
menescalnickolas <menescalnickolas@gmail.com>
2024-06-22 21:11:15 +0200

    Added const movies and useState( )

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {id: 1, title: "Challengers"},
+    {id: 2, title: "Easy A"},
+    {id: 3, title: "Clue"}
+  ]);
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
   return (
     <div>
-      <div>Movie 1</div>
-      <div>Movie 2</div>
-      <div>Movie 3</div>
+      {movies.map((movie) => {
+        return <div key={movie.id}>{movie.title}</div>;
+      })}
     </div>
   );
 };
