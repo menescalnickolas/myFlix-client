4a98eca7bd7c71e07f6f2cfef73861582383b559
menescalnickolas <menescalnickolas@gmail.com>
2024-06-22 21:35:38 +0200

    Added back button to MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,10 +1,11 @@
-export const MovieView = ({ movie }) => {
+export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
         <span>Title: </span>
         <span>{movie.title}</span>
       </div>
+      <button onClick={onBackClick}>Back</button>
     </div>
   );
 };
