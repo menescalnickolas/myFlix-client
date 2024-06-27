761552c4cb9ab81f2808fad00a07d8b1a6f31b19
menescalnickolas <menescalnickolas@gmail.com>
2024-06-27 23:04:47 +0200

    Created login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,13 +1,53 @@
-export const LoginView = () => {
+import React, { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();// this prevents the default behavior of the form which is to reload the entire page
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://testflix2-2b11acffaf24.herokuapp.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then((response) => response.json())
+    .then((data) => {
+      console.log("Login response: ", data);
+      if (data.user) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("token", data.token);
+        onLoggedIn(data.user, data.token);
+      } else {
+        alert("There's no such user!");
+      }
+    })
+    .catch((e) => {
+      alert("Something went wrong!");
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input type="text" />
+        <input type="text" value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          minLength="5"
+          required/>
       </label>
       <label>
         Password:
-        <input type="password" />
+        <input type="password" value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required/>
       </label>
       <button type="submit">Submit</button>
     </form>
