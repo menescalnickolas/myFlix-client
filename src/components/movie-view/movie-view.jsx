d630e92ba47aa44f99e163a02585ae6297216cfe
menescalnickolas <menescalnickolas@gmail.com>
2024-06-29 16:13:09 +0200

    Updated movie.view.jsx to include movie poster

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,6 +3,9 @@ export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
+        <img src={movie.image} alt={movie.tile} />
+      </div>
+      <div>
         <span>Title: </span>
         <span>{movie.title}</span>
       </div>
