fe21e0e4666c6160f2e6b34b3843aa8c4a456c5f
menescalnickolas <menescalnickolas@gmail.com>
2024-07-08 19:52:46 +0200

    Updated navigation-bar

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/search?query=${searchQuery}`);
+  };
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          MyTestFlix
+          MYTESTFLIX
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -27,9 +37,14 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                   Home
                 </Nav.Link>
                 <Nav.Link as={Link} to="/profile">
-                    Profile
-                  </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                  Profile
+                </Nav.Link>
+              <Nav.Link onClick={onLoggedOut} className="btn logoutbutton">Logout</Nav.Link>
+                  <form className="d-flex" role="search" onSubmit={handleSearchSubmit}>
+                    <input className="form-control me-2" type="search" placeholder="Search movie" aria-label="Search" value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}></input>
+                    <button className="btn" type="submit">Search</button>
+                  </form>
               </>
             )}
           </Nav>
