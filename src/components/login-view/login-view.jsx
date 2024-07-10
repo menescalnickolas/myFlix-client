5dc66115c7faf41a6823b023bd455de5641f3e30
menescalnickolas <menescalnickolas@gmail.com>
2024-07-10 16:44:25 +0200

    Added line breaks

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -46,15 +46,15 @@ const [password, setPassword] = useState("");
           minLength="5"
           required/>
       </Form.Group>
-      
+      <br></br>
       <Form.Group controlId="formPassword"> 
         <Form.Label>Password: </Form.Label>
         <Form.Control type="password" value={password}
         onChange={(e) => setPassword(e.target.value)}
         required/>
       </Form.Group>
-      
-      <Button variant="primary" type="submit">
+      <br></br>
+      <Button variant="light" type="submit">
         Submit
       </Button>
     </Form>
