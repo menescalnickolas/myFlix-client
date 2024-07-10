ea078bcaa3cce65c57b5a3a57d675eab3231a58f
menescalnickolas <menescalnickolas@gmail.com>
2024-07-10 16:44:09 +0200

    Delete commented out code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -27,40 +27,6 @@ export const MovieCard = ({ movie, user, onToggleFavorite }) => {
     onToggleFavorite(movie._id);
   }
   
-  /*
-    try {
-      let response;
-      if (isFavorite) {
-        response = await fetch(`https://testflix2-2b11acffaf24.herokuapp.com/users/${encodeURIComponent(user.Username)}/movies/${encodeURIComponent(movie._id)}`, 
-      {
-        method: "DELETE",
-          headers: {
-            Authorization: `Bearer ${storedToken}`,
-          }
-      }
-      ), window.location.reload();
-        console.log("Removed from favorites:", response.data);
-      } else {
-        response = await fetch(`https://testflix2-2b11acffaf24.herokuapp.com/users/${encodeURIComponent(user.Username)}/movies/${encodeURIComponent(movie._id)}`,
-        {
-          method: "POST",
-            headers: {
-              Authorization: `Bearer ${storedToken}`,
-            }
-        }
-      ),
-      window.location.reload();
-        console.log("Added to favorites:", response.data);
-      }
-      const updatedUser = await response.json();
-      setIsFavorite(!isFavorite);
-      setUser(updatedUser);  // Update user state with the response data
-    } catch (error) {
-      console.error("There was an error toggling the favorite status!", error);
-    }
-  };
-  */
-
   return (
   <Link to={`/movies/${encodeURIComponent(movie._id)}`} className="link">
     <Card className="h-100" style={{ cursor: "pointer" }}>
